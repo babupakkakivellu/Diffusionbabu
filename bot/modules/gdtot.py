76cd5e768577d9a5ca98637e433d5db03c6f1859
fine008 <115710749+fine008@users.noreply.github.com>
2023-03-10 17:17:21 +0530

    Update gdtot.py

--- bot/modules/gdtot.py
+++ bot/modules/gdtot.py
@@ -35,8 +35,7 @@ def search_gdtot(update, context):
             link = x['href']
             if 'gdbot.xyz' not in link:
                 text += f"<a href='{link}'><b>{str(urlparse(link).hostname).upper()}</b></a> "
-        editMessage(txt)
-            asleep(1.5)
+            editMessage(txt)
             if len(text) > 4000:
                 sendMessage("text, context.bot, update.message)
                 text = ""
