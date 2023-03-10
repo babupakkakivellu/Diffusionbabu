1b3830dc1240c1c9ab1301d6785bdacff898d9e8
fine008 <115710749+fine008@users.noreply.github.com>
2023-03-10 13:07:54 +0530

    Create gdtot.py

---
+++ bot/modules/gdtot.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from re import compile as re_compile
+from requests import get as rget
+from urllib.parse import urlparse, quote_plus
+
+
+def search_gdtot(query):
+    resp =  rget(f'https://gdbot.xyz/search?q={quote_plus(query)}')
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    links = soup.select("a[href*='https://gdbot.xyz/file']")
+    info = [x.string for x in soup.find_all('span', string=re_compile(r'Size*'))]
+    titles = [x.string for x in soup.find_all('a')[5:]]
+    text, result = '', []
+    for i, (title, inf, link) in enumerate(zip(titles, info, links), start=1):
+        soup = BeautifulSoup(rget(link['href']).text, 'html.parser')
+        text += f"{str(i).zfill(3)}. <a href='{link['href']}'>{str(title).strip()}</a>\n{inf}\n"
+        for x in soup.select('a'):
+            link = x['href']
+            if 'gdbot.xyz' not in link:
+                text += f"<a href='{link}'><b>{str(urlparse(link).hostname).upper()}</b></a> "
+        text += '\n\n'
+        result.append(text)
+        text = ''
+    return result
