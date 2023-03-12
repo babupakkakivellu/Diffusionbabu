13dda353cd222c0a409ecdb0ab6542abbcc76166
fine008 <115710749+fine008@users.noreply.github.com>
2023-03-12 13:05:57 +0530

    Update gdtot.py

--- bot/modules/gdtot.py
+++ bot/modules/gdtot.py
@@ -24,13 +24,9 @@ def search_gdtot(update, context):
     info = [x.string for x in soup.find_all('span', string=re_compile(r'Size*'))]
     titles = [x.string for x in soup.find_all('a')[5:]]
     text = ''
-    for i, (title, inf, link) in enumerate(zip(titles, info, links), start=1):
+    for i, (title, inf, link) in enumerate(zip(titles, info, links), start=3):
         soup = BeautifulSoup(rget(link['href']).text, 'html.parser')
         text += f"{str(i).zfill(3)}. <a href='{link['href']}'>{str(title).strip()}</a>\n{inf}\n"
-        for x in soup.select('a'):
-            link = x['href']
-            if 'gdbot.xyz' not in link:
-                text += f"<a href='{link}'><b>{str(urlparse(link).hostname).upper()}</b></a> "
         text += '\n\n'
         sendMessage(text, context.bot, update.message)
         text = ""
